c3552304300be1fd96518027670b21a650a5c474
Dante <motiv8lab@gmail.com>
2021-01-12 22:31:40 -0500

    14

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -2,8 +2,8 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types";
 import Image from "next/image";
 let client = require("contentful").createClient({
-  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+  space: process.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.NEXT_CONTENTFUL_ACCESS_TOKEN,
 });
 
 export async function getStaticPaths() {
