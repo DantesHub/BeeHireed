3ef097e1d6dede2c677b441474e4087da79e7a07
Dante <motiv8lab@gmail.com>
2021-01-12 22:22:59 -0500

    tried stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,7 @@
 import "../styles/globals.css";
-import "../styles/BlogStyles.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "../styles/uikit.min.css";
-import Head from "next/head";
-import Navbar from "../components/Navbar";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    console.log("calling");
-    const script = document.createElement("script");
-    script.src =
-      "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.8/js/uikit.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-  library.add(fab);
-  return (
-    <div>
-      <Head>
-        <script src='https://f.convertkit.com/ckjs/ck.5.js'></script>
-      </Head>
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,228 +1,44 @@
-import React from "react";
-import Image from "next/image";
 import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
-const landing = () => {
+let client = require("contentful").createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: "article",
+  });
+  return {
+    props: {
+      articles: data.items,
+    },
+    revalidate: 30,
+  };
+}
+
+export default function Home({ articles }) {
+  console.log(articles);
   return (
-    <React.Fragment>
-      <div
-        className='uk-position-relative uk-visible-toggle'
-        data-uk-slider='finite: true'
-      >
-        <ul
-          className='uk-slider-items uk-child-width-1-2@m uk-grid uk-grid-small'
-          data-uk-height-viewport
-        >
-          <li className='uk-transition-toggle uk-animation-toggle overflow-hidden sm-hidden'>
-            <div className='uk-position-center'>
-              <div className='width-100 margin-left-auto sm-width-100'>
-                <img
-                  src='images/backgrounds/bg-01.png'
-                  alt='job application automation'
-                />
-              </div>
-            </div>
-          </li>
+    <div>
+      <Head>
+        <title>Next.js + Contentful</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-          <li className='uk-transition-toggle uk-animation-toggle overflow-hidden'>
-            <div className='uk-position-cover'>
-              <div className='uk-position-center  text-left'>
-                <div className='width-70 margin-auto'>
-                  <h2 className='text-weight-700 text-gray-dark bee-title'>
-                    Stay connected with your friends with softop.
-                  </h2>
-                  <p className='text-gray-regular'>
-                    The project management software that keeps teams going.
-                    Collaborate as if you were right next to each other.
-                  </p>
-
-                  <img
-                    className='margin-bottom-25px'
-                    src='images/separator.png'
-                    alt=''
-                  />
-
-                  <div className='form-group'>
-                    <input
-                      className='form-control form_lastname bg-transparent no-border-top no-border-left no-border-right border-color-gray-extra-light border-3px padding-10px text-gray-dark text-weight-400 width-70 sm-width-100'
-                      type='text'
-                      name='name'
-                      placeholder='Email'
-                      required='required'
-                      data-error='Lastname is required.'
-                    />
-                  </div>
-
-                  <a
-                    className='btn btn-large btn-gold sm-display-table no-margin-bottom'
-                    href='#'
-                  >
-                    Subscribe
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
+      <main>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.sys.id}>
+              <Link href={"/articles/" + article.fields.slug}>
+                <a>{article.fields.title}</a>
+              </Link>
+            </li>
+          ))}
         </ul>
-      </div>
-      <a
-        className='uk-position-center-left uk-position-small uk-hidden-hover left-minus-50'
-        href='#'
-        data-uk-slidenav-previous
-        data-uk-slider-item='previous'
-      ></a>
-      <a
-        className='uk-position-center-right uk-position-small uk-hidden-hover right-minus-50'
-        href='#'
-        data-uk-slidenav-next
-        data-uk-slider-item='next'
-      ></a>
-      {/* Services */}
-      <div
-        id='features'
-        className='padding-top-bottom-150px bg-gray-transparent'
-      >
-        <div className='container sm-container-spread'>
-          <div className='row'>
-            <div className='col-md-8 col-sm-12 col-xs-12 center-col text-center margin-auto'>
-              <h2 className='text-weight-700 text-gray-dark'>
-                Check out the features.
-              </h2>
-
-              <p className='text-gray-regular text-center'>
-                The project management software that lorem ipsum and best to
-                know that because it keeps teams going. Collaborate as if you
-                were right next to each other.
-              </p>
-
-              <img className='margin-auto' src='images/separator.png' alt='' />
-            </div>
-          </div>
-          <div className='row margin-top-75px'>
-            <div className='col-md-4 col-sm-12 col-xs-12 center-col text-left margin-auto sm-margin-top-75px'>
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-right margin-bottom-75px sm-text-center'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: move; ratio: 1.2'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    Giveaways.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-right margin-bottom-75px sm-text-center'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: world; ratio: 1.2'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    Global Stuff.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-right sm-text-center sm-margin-bottom-75px'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: database; ratio: 1.2'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    Databases.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className='col-md-4 col-sm-12 col-xs-12 center-col text-left margin-auto md-text-center'>
-              <div className='sm-container-spread md-text-center'>
-                <div className='width-100 margin-left-auto sm-width-100'>
-                  <Image
-                    src='/images/backgrounds/bg-02.png'
-                    alt=''
-                    width='350'
-                    height='780'
-                  />
-                </div>
-              </div>
-            </div>
-            <div className='col-md-4 col-sm-12 col-xs-12 center-col text-left margin-auto sm-margin-top-75px'>
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-left margin-bottom-75px sm-text-center'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: receiver; ratio: 1.5'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    24/7 support.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-left margin-bottom-75px sm-text-center'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: pull; ratio: 1.5'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    Unlimited Downloads.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-              <div className='row'>
-                <div className='col-md-12 col-sm-12 col-xs-12 text-left sm-text-center'>
-                  <span
-                    className='text-gray-dark bg-gold width-50px height-50px line-height-50 text-center border-radius-100'
-                    data-uk-icon='icon: cog; ratio: 1.5'
-                  ></span>
-
-                  <h6 className='text-gray-dark text-extra-large text-weight-700 margin-top-25px'>
-                    1 Month Trial.
-                  </h6>
-
-                  <p className='text-gray-dark no-margin md-text-center'>
-                    As opposed to using 'Content here, content here', making it
-                    look like readable English.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
+      </main>
+    </div>
   );
-};
-
-export default landing;
+}
